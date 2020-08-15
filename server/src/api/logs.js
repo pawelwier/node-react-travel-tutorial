43c5e8580d9c55f12f3bb9f99f5769236801bb39
Paweł Wierciński <47865579+pawelwier@users.noreply.github.com>
2020-08-15 13:19:55 +0200

    + front

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -4,15 +4,26 @@ const LogEntry = require('../models/LogEntry');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        message: 'zxcv'
-    })
+router.get('/', async (req, res, next) => {
+    try {
+        const entries = await LogEntry.find();
+        res.json(entries);
+    } catch (err) {
+        next(err);
+    }
 })
 
-router.post('/', (req, res) => {
-    const logEntry = new LogEntry(req.body);
-    console.log(req.body);
+router.post('/', async (req, res, next) => {
+    try {
+        const logEntry = new LogEntry(req.body);
+        const createdEntry = await logEntry.save();
+        res.json(createdEntry);
+    } catch (err) {
+        if (err.name === 'ValidationError') {
+            res.status(422);
+        }
+        next(err);
+    }
 });
 
 module.exports = router;
