b80c944079258aa736a6271259c029452ad64f20
Paweł Wierciński <47865579+pawelwier@users.noreply.github.com>
2020-08-15 13:56:58 +0200

    + front 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,44 @@
-import React, { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import React, { useState, useEffect } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+
+import { listLogEntries } from './API';
 
 const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
+    latitude: 4.7577,
+    longitude: 20.4376,
+    zoom: 3
   });
+
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries();
+      setLogEntries(logEntries);
+    })();
+  }, []);
 
   return (
     <ReactMapGL
       {...viewport}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={setViewport}
-    />
+    >
+      {
+        logEntries.map(entry => (
+          <Marker
+            key = {entry._id}
+            latitude={entry.latitude}
+            longitude={entry.longitude}
+            offsetLeft={-20}
+            offsetTop={-10}>
+          <div>{entry.title}</div>
+          </Marker>
+        ))
+      }
+    </ReactMapGL>
   );
 }
 
