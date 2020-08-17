b21a368b6c23976a03d7736ade6a2d4809164cc1
Paweł Wierciński <47865579+pawelwier@users.noreply.github.com>
2020-08-17 21:07:09 +0200

    + front 3

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 
 import { listLogEntries } from './API';
 
 const App = () => {
   const [logEntries, setLogEntries] = useState([]);
+  const [showPopup, setShowPopup] = useState({});
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -28,14 +29,44 @@ const App = () => {
     >
       {
         logEntries.map(entry => (
-          <Marker
-            key = {entry._id}
-            latitude={entry.latitude}
-            longitude={entry.longitude}
-            offsetLeft={-20}
-            offsetTop={-10}>
-          <div>{entry.title}</div>
-          </Marker>
+          <>
+            <Marker
+              key = {entry._id}
+              latitude={entry.latitude}
+              longitude={entry.longitude} 
+            >
+            <div
+              onClick={() => {
+                setShowPopup({
+                  ...showPopup,
+                  [entry._id]: true
+                })
+              }}  
+            ><img src="https://i.imgur.com/y0G5YTX.png" style={{width:"30px",height:"30px"}}/></div>
+            </Marker>
+            {
+              showPopup[entry._id] ? (
+                <Popup
+                  latitude={entry.latitude}
+                  longitude={entry.longitude}
+                  closeButton={true}
+                  closeOnClick={false}
+                  onClose={() => {
+                    setShowPopup({
+                      ...showPopup,
+                      [entry._id]: false
+                    })
+                  }}
+                  anchor="top">
+                  <div>
+                    <h3>{entry.title}</h3>
+                    <p>{entry.comments}</p>
+                    <small>Visited on: {new Date(entry.updatedAt).toLocaleDateString()}</small>
+                  </div>
+                </Popup>
+              ) : null
+            }
+          </>
         ))
       }
     </ReactMapGL>
